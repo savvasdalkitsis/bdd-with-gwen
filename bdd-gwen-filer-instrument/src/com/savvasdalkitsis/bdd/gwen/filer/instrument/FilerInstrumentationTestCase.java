eefa85dd6f6f22e15815945dc85999c0744753b0
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2013-12-15 15:21:50 +0000

    Created instrumentation project.

---
+++ bdd-gwen-filer-instrument/src/com/savvasdalkitsis/bdd/gwen/filer/instrument/FilerInstrumentationTestCase.java
@@ -0,0 +1,6 @@
+package com.savvasdalkitsis.bdd.gwen.filer.instrument;
+
+import android.test.InstrumentationTestCase;
+
+public class FilerInstrumentationTestCase extends InstrumentationTestCase {
+}
---
+++ bdd-gwen-filer-instrument/src/com/savvasdalkitsis/bdd/gwen/filer/instrument/tests/ExampleTest.java
@@ -0,0 +1,10 @@
+package com.savvasdalkitsis.bdd.gwen.filer.instrument.tests;
+
+import com.savvasdalkitsis.bdd.gwen.filer.instrument.FilerInstrumentationTestCase;
+
+public class ExampleTest extends FilerInstrumentationTestCase {
+
+    public void testExample() {
+
+    }
+}
